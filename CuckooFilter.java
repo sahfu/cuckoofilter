ce9d076fdcca3318cf5bdf00b6cd0c6488974163
Cong Chung <zhicongchung@gmail.com>
2015-10-15 21:17:28 +0800

    fix bug
    fix bug

--- CuckooFilter.java
+++ CuckooFilter.java
@@ -3,7 +3,7 @@ import java.util.Random;
 /**
  * A Java implemetion of Cuckoo Filter
  */
-public class CukkoFilter {
+public class CuckooFilter {
 
     static final int MAXIMUM_CAPACITY = 1 << 30;
 
@@ -13,8 +13,9 @@ public class CukkoFilter {
     private Bucket[] buckets;
     private Random random;
 
-    public CukkoFilter(int capacity) {
-        this.capacity = tableSizeFor(capacity);
+    public CuckooFilter(int capacity) {
+        capacity = tableSizeFor(capacity);
+        this.capacity = capacity;
         buckets = new Bucket[capacity];
         random = new Random();
         for (int i = 0; i < capacity; i++) {
@@ -94,19 +95,20 @@ public class CukkoFilter {
 
     private byte fingerprint(Object o) {
         int h = o.hashCode();
-        h = (h + 0x7ed55d16) + (h << 12);
-        h = (h ^ 0xc761c23c) ^ (h >> 19);
-        h = (h + 0x165667b1) + (h << 5);
-        h = (h + 0xd3a2646c) ^ (h << 9);
-        h = (h + 0xfd7046c5) + (h << 3);
-        h = (h ^ 0xb55a4f09) ^ (h >> 16);
-        if (h == Bucket.NULL_FINGERPRINT)
-            h = 40;
-        return (byte) h;
+        h += ~(h<<15);
+        h ^=  (h>>10);
+        h +=  (h<<3);
+        h ^=  (h>>6);
+        h += ~(h<<11);
+        h ^=  (h>>16);
+        byte hash = (byte) h;
+        if (hash == Bucket.NULL_FINGERPRINT)
+            hash = 40;
+        return hash;
     }
 
     public int hash(Object key) {
-        int h = hashint(key.hashCode());
+        int h = key.hashCode();
         h -= (h << 6);
         h ^= (h >> 17);
         h -= (h << 9);
@@ -117,15 +119,9 @@ public class CukkoFilter {
         return h & (capacity - 1);
     }
 
-    public static int hashint(int a) {
-
-        return a;
-    }
-
     public boolean isEmpty() {
         return size == 0;
     }
-
 
     static final int tableSizeFor(int cap) {
         int n = cap - 1;
